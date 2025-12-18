eb31ba3adbfe5dee4353a774224aa3fa3495a60d
yuvarajsaravanan21 <yuvaraj.saravanan2023@vitstudent.ac.in>
2025-12-18 16:57:11 +0530

    added feild to add author name and print it

--- lib.py
+++ lib.py
@@ -1,6 +1,9 @@
 n=int(input("Enter number of books:"))
 books=[]
+author=[]
 for i in range (n):
-    books.append(input("Enter book name: "))
-for i in books:
-    print(i,end="\n")
+    books.append(input("Enter Book Name: "))
+    author.append(input("Enter Author Name"))
+print("BOOK",'  ','Author')
+for i in range(n):
+    print(books[i],"    ",author[i],end="\n")
