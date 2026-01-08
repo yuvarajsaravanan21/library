ebd2c92259696815434e885509259fa76c9617d5
yuvarajsaravanan21 <yuvaraj.saravanan2023@vitstudent.ac.in>
2026-01-08 17:02:13 +0530

    update by per1

--- login.py
+++ login.py
@@ -10,6 +10,6 @@ password = input("Enter password: ")
 
 # Login check
 if username == correct_username and password == correct_password:
-    print("✅ Login successful")
+    print("✅ Login ok") #change by per1
 else:
     print("❌ Invalid username or password")
